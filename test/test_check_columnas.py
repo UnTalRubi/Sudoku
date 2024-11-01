7f4c7b8143187b3a7468e1ecef8ff39cde5b597e
Rubi <untalrubi@gmail.com>
2024-11-01 13:30:39 +0100

    Markers, PH check_cuadrado + check_filas, check_columnas done
    Añadido el archivo de pytest con los markers a utilizar. Añadidos placeholders para los scripts de check_cuadrado y check_filas. Script de check_columnas terminado y testeado correctamente.

--- test/test_check_columnas.py
+++ test/test_check_columnas.py
@@ -2,23 +2,34 @@ import pytest
 from src.check_columnas import check_columnas
 import casosTest.casos_test_sudoku as casos_test
 
-def test_check_columnas(sudoku, sano):
-    assert check_columnas(sudoku) == sano
+@pytest.mark.columas
+def test_check_columnas():
+    assert check_columnas (casos_test.correcto) == True
 
-test_check_columnas(casos_test.correcto, True)
+@pytest.mark.columas
+def test_check_columnas():
+    assert check_columnas (casos_test.numero_repetido_fila_columna) == False
 
-test_check_columnas(casos_test.numero_repetido_fila_columna, False)
+@pytest.mark.columas
+def test_check_columnas():
+    assert check_columnas (casos_test.numero_no_presente) == False
 
-test_check_columnas(casos_test.numero_repetido_columna, False)
+@pytest.mark.columas
+def test_check_columnas():
+    assert check_columnas (casos_test.numero_fuera_del_rango) == False
 
-test_check_columnas(casos_test.numero_no_presente, False)
+@pytest.mark.columas
+def test_check_columnas():
+    assert check_columnas (casos_test.caracteres) == True
 
-test_check_columnas(casos_test.numero_fuera_del_rango, False)
+@pytest.mark.columas
+def test_check_columnas():
+    assert check_columnas (casos_test.numeros_reales) == True
 
-test_check_columnas(casos_test.caracteres, True)
+@pytest.mark.columas
+def test_check_columnas():
+    assert check_columnas (casos_test.irregular_fila) == True
 
-test_check_columnas(casos_test.numeros_reales, True)
-
-test_check_columnas(casos_test.irregular_fila, True)
-
-test_check_columnas(casos_test.irregular_columna, True)
+@pytest.mark.columas
+def test_check_columnas():
+    assert check_columnas (casos_test.irregular_columna) == False
