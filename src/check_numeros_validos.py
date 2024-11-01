9baad8ddfe549b5da8903d7e5d03c69dcd9bf693
Rubi <untalrubi@gmail.com>
2024-11-01 19:08:15 +0100

    Check_numeros_validos
    Creado script de números válidos que determina si el número es un int y si está dentro de los números que puede tomar en el sudoku.

--- src/check_numeros_validos.py
+++ src/check_numeros_validos.py
@@ -1,2 +1,37 @@
-def check_numeros_validos():
-    pass
+def check_numeros_enteros(sudoku):
+    
+    for fila in sudoku:
+        for numero in fila:
+            if not isinstance(numero, int):
+                return False
+            
+    return True
+
+def check_numeros_rango(sudoku):
+
+    numeros_en_rango = range(1, len(sudoku) + 1)
+
+    for fila in sudoku:
+        for numero in fila:
+            if numero not in numeros_en_rango:
+                return False
+            
+    return True
+
+def check_numeros_validos(sudoku):
+
+    return check_numeros_enteros(sudoku) and check_numeros_rango(sudoku)
+
+if __name__ == '__main__':
+
+    import sys
+    sys.path.append('..')
+
+    import casosTest.casos_test_sudoku as casosTest
+
+    for attr in sorted(casosTest.__dict__):
+        if attr.startswith('__'):
+            pass
+            # Skip atributo
+        else:
+            print(attr, " => ", check_numeros_validos(casosTest.__dict__[attr]))
